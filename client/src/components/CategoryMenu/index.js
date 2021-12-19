cdf9b5cf01b4ddd40d5b3ae48994f3e53eb229e8
colton bloomquist <coltonbloomquist@yahoo.com>
2021-12-18 19:14:01 -0700

    updated globalState, productList, Cart, and CategoryMenu to use redux

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,41 +1,41 @@
-import React, { useEffect } from 'react';
-import { useQuery } from '@apollo/client';
-import { useStoreContext } from '../../utils/GlobalState';
+import React, { useEffect } from "react";
+import { useQuery } from "@apollo/client";
+import { useStoreContext } from "../../utils/GlobalState";
 import {
   UPDATE_CATEGORIES,
   UPDATE_CURRENT_CATEGORY,
-} from '../../utils/actions';
-import { QUERY_CATEGORIES } from '../../utils/queries';
-import { idbPromise } from '../../utils/helpers';
+} from "../../utils/actions";
+import { QUERY_CATEGORIES } from "../../utils/queries";
+import { idbPromise } from "../../utils/helpers";
 
 function CategoryMenu() {
-  const [state, dispatch] = useStoreContext();
+  const store = useStoreContext();
 
-  const { categories } = state;
+  const { categories } = store.getState();
 
   const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
 
-  useEffect(() => {
+  store.subscribe(() => {
     if (categoryData) {
-      dispatch({
+      store.dispatch({
         type: UPDATE_CATEGORIES,
         categories: categoryData.categories,
       });
       categoryData.categories.forEach((category) => {
-        idbPromise('categories', 'put', category);
+        idbPromise("categories", "put", category);
       });
     } else if (!loading) {
-      idbPromise('categories', 'get').then((categories) => {
-        dispatch({
+      idbPromise("categories", "get").then((categories) => {
+        store.dispatch({
           type: UPDATE_CATEGORIES,
           categories: categories,
         });
       });
     }
-  }, [categoryData, loading, dispatch]);
+  });
 
   const handleClick = (id) => {
-    dispatch({
+    store.dispatch({
       type: UPDATE_CURRENT_CATEGORY,
       currentCategory: id,
     });
--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,37 +1,56 @@
-import React, { useEffect } from 'react';
-import ProductItem from '../ProductItem';
-import { useStoreContext } from '../../utils/GlobalState';
-import { UPDATE_PRODUCTS } from '../../utils/actions';
-import { useQuery } from '@apollo/client';
-import { QUERY_PRODUCTS } from '../../utils/queries';
-import { idbPromise } from '../../utils/helpers';
-import spinner from '../../assets/spinner.gif';
+import React, { useEffect } from "react";
+import ProductItem from "../ProductItem";
+import { useStoreContext } from "../../utils/GlobalState";
+import { UPDATE_PRODUCTS } from "../../utils/actions";
+import { useQuery } from "@apollo/client";
+import { QUERY_PRODUCTS } from "../../utils/queries";
+import { idbPromise } from "../../utils/helpers";
+import spinner from "../../assets/spinner.gif";
 
 function ProductList() {
-  const [state, dispatch] = useStoreContext();
-
+  const store = useStoreContext();
+  const state = store.getState();
   const { currentCategory } = state;
 
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  useEffect(() => {
+  store.subscribe(() => {
     if (data) {
-      dispatch({
+      store.dispatch({
         type: UPDATE_PRODUCTS,
         products: data.products,
       });
       data.products.forEach((product) => {
-        idbPromise('products', 'put', product);
+        idbPromise("products", "put", product);
       });
     } else if (!loading) {
-      idbPromise('products', 'get').then((products) => {
-        dispatch({
+      idbPromise("products", "get").then((products) => {
+        store.dispatch({
           type: UPDATE_PRODUCTS,
           products: products,
         });
       });
     }
-  }, [data, loading, dispatch]);
+  });
+
+  // useEffect(() => {
+  //   if (data) {
+  //     store.dispatch({
+  //       type: UPDATE_PRODUCTS,
+  //       products: data.products,
+  //     });
+  //     data.products.forEach((product) => {
+  //       idbPromise("products", "put", product);
+  //     });
+  //   } else if (!loading) {
+  //     idbPromise("products", "get").then((products) => {
+  //       store.dispatch({
+  //         type: UPDATE_PRODUCTS,
+  //         products: products,
+  //       });
+  //     });
+  //   }
+  // }, [data, loading, store.dispatch]);
 
   function filterProducts() {
     if (!currentCategory) {
